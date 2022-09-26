a98aab2556c4d29872d006409b5e9cb26e6e117d
sara khalil <sarakhalil3377@gmail.com>
2022-09-26 18:27:42 +0300

    DONE

--- index.js
+++ index.js
@@ -14,16 +14,17 @@ class Circle {
     get area() {
         return Math.PI * (this.radius ** 2)
 
-    }//wata shtek bka nrxa aslakay radious betawa loya peshtr *2 man krd esta /2 dakain 
+    }
     set diameter(diameter) {
         this.radius = diameter / 2
     }
 
-    set circumference(circumference) {
-        this.radius = circumference / Math.PI * 2
+    set circumference (newCircumference) {
+        this.radius = (newCircumference/Math.PI)/2
     }
 
     set area(area) {
         this.radius = Math.sqrt(area / Math.PI)
     }
-}//new zyad bka bo parameterakab
+}
+//
